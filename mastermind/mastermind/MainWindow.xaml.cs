6a33966827d6285a5b2fa51fc614eb039c9cbe0f
mahsae98 <11901872@student.pxl.be>
2024-11-17 05:15:40 +0100

    MasterMind-04

--- mastermind/mastermind/MainWindow.xaml.cs
+++ mastermind/mastermind/MainWindow.xaml.cs
@@ -17,10 +17,10 @@ namespace mastermind
     public partial class MainWindow : Window
     {
         private readonly string[] codeColors = { "Red", "Orange", "Yellow", "Green", "White", "Blue" };
-        private string[] generatedCode;
+        private string[] generatedCode = new string[4];
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();      
             GenerateRandomCode();
             
         }
@@ -34,8 +34,70 @@ namespace mastermind
                 generatedCode[i] = codeColors[random.Next(codeColors.Length)];
             }
 
-            this.Title = "MasterMind  /  Code: ( " + string.Join(" , ", generatedCode)+" )"; 
+            Title = "MasterMind  /  Code: ( " + string.Join(" , ", generatedCode)+" )"; 
         }
 
+        private Brush BrushColor(string colorName)
+
+        {
+            switch (colorName)
+            {
+                case "Red":
+                    return Brushes.Red;
+                case "Orange":
+                    return Brushes.Orange;
+                case "Yellow":
+                    return Brushes.Yellow;
+                case "Green":
+                    return Brushes.Green;
+                case "White":
+                    return Brushes.White;
+                case "Blue":
+                    return Brushes.Blue;
+                default:
+                    return Brushes.Transparent;
+
+
+            }
+        }
+
+            private void color1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (color1.SelectedItem is ComboBoxItem selectedItem && selectedItem.Content is string colorName) 
+            {
+                color1Label.Background = BrushColor(colorName);
+            
+            }
+        }
+
+        private void color2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (color2.SelectedItem is ComboBoxItem selectedItem && selectedItem.Content is string colorName)
+            {
+                color2Label.Background = BrushColor(colorName);
+            }
+
+        }
+
+        private void color3_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (color3.SelectedItem is ComboBoxItem selectedItem && selectedItem.Content is string colorName)
+            {
+                color3Label.Background = BrushColor(colorName);
+            }
+        }
+
+        private void color4_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (color4.SelectedItem is ComboBoxItem selectedItem && selectedItem.Content is string colorName)
+            {
+                color4Label.Background = BrushColor(colorName);
+            }
+        }
+        
+
+       
+                
+    
     }
 }
